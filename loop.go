6d2f62e77f14d75686227fa3f27632189879a06a
Natee <bin.zhang@yuyidata.com>
2020-03-16 16:50:16 +0800

    基础语法

---
+++ loop.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(
+		converToBin(5),
+		converToBin(12),
+		converToBin(13))
+	printFile("abc.txt")
+	a, b := 3, 4
+	swap(&a, &b)
+	fmt.Println(a, b)
+}
+
+func converToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
+
+func swap(a, b *int) {
+	*b, *a = *a, *b
+}

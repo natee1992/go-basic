b5922f30b0f2715296eabeeaa7f14461d3cd9252
Natee <bin.zhang@yuyidata.com>
2020-03-18 13:52:18 +0800

    basic end

---
+++ norepeating.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/*
+寻找最长不含有重复字符的子串
+对于每一个字母x：
+	- lastOccurred[x] 不存在，或者< start 无需操作
+	- lastOccurred[x] >= start   更新start
+	- 更新lastOccurred[x] 更新maxLength
+ */
+func main() {
+	fmt.Println(lengthOfNonRepeatingSubStr("abab"))
+	fmt.Println(lengthOfNonRepeatingSubStr("你好"))
+}
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	lastOccured := make(map[byte]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		lastI, ok := lastOccured[ch]
+		if ok && lastI >= start {
+			start = lastOccured[ch] + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccured[ch] = i
+	}
+	return maxLength
+}
---
+++ strings.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "yes你好时间表!"
+	fmt.Println(len(s))
+	for _, b := range []byte(s) {
+		fmt.Printf("%X ", b)
+	}
+	fmt.Printf("%X\n", []byte(s))
+
+	for i, ch := range s { // ch is a rune
+		fmt.Printf("(%d %X)", i, ch)
+	}
+	fmt.Println("\n Rune count:", utf8.RuneCountInString(s))
+	bytes := []byte(s)
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes)
+		bytes = bytes[size:]
+		fmt.Printf("%c ",ch)
+	}
+}
+
+
